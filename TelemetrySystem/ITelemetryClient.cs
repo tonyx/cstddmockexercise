4def20d947db28c5fb4e0f71c92812d7125ff0f1
Tonyx <tonyx@yahoo.com>
2011-02-09 01:49:59 +0100

    telemetry interface and test

---
+++ TelemetrySystem/ITelemetryClient.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tonino
+ * Date: 09/02/2011
+ * Time: 00:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TDDMicroExercises.TelemetrySystem
+{
+	/// <summary>
+	/// Description of ITelemetryClient.
+	/// </summary>
+	public interface ITelemetryClient
+	{
+		
+	}
+}
