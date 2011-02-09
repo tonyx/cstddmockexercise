47a5490a122b03915fcec35cb0a1f1417ed3f7dd
Tonyx <tonyx@yahoo.com>
2011-02-09 05:21:15 +0100

    ticket

---
+++ TurnTicketDispenser.Tests/TurnTicketDispenserTest.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tonino
+ * Date: 09/02/2011
+ * Time: 02:09
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using NUnit.Framework;
+
+namespace TDDMicroExercises.TurnTicketDispenser.Tests
+{
+	[TestFixture]
+	public class TurnTicketDispenserTest
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+		}
+	}
+}
---
+++ TurnTicketDispenser/ITurnNumberSequence.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tonino
+ * Date: 09/02/2011
+ * Time: 04:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TDDMicroExercises.TurnTicketDispenser
+{
+	/// <summary>
+	/// Description of ITurnNumberSequance.
+	/// </summary>
+	public interface ITurnNumberSequence
+	{
+		 int GetNextNumber();		
+	}
+}
