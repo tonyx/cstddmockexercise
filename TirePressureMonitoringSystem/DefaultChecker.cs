95526583e3b6979f154f29dd7c088c6ec2470706
Tonyx <tonyx@yahoo.com>
2011-02-10 23:02:59 +0100

    alarm can check according to a custom checker

---
+++ TirePressureMonitoringSystem/DefaultChecker.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tonino
+ * Date: 10/02/2011
+ * Time: 22:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+	/// <summary>
+	/// Description of DefaultChecker.
+	/// </summary>
+	public class DefaultChecker
+	{
+		public DefaultChecker()
+		{
+		}
+	}
+}
---
+++ TirePressureMonitoringSystem/IChecker.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tonino
+ * Date: 10/02/2011
+ * Time: 22:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+	/// <summary>
+	/// Description of IChecker.
+	/// </summary>
+	public interface IChecker
+	{
+		
+	}
+}
