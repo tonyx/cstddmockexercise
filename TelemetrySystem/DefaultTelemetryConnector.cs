91abccc608a8d020b137929496627e12a48deab2
Tonyx <tonyx@yahoo.com>
2011-02-11 18:00:05 +0100

    starting communication and connection responsabilities splitting

---
+++ TelemetrySystem/DefaultTelemetryConnector.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tonino
+ * Date: 11/02/2011
+ * Time: 17:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TDDMicroExercises.TelemetrySystem
+{
+	/// <summary>
+	/// Description of DefaultTelemetryConnector.
+	/// </summary>
+	public class DefaultTelemetryConnector
+	{
+		public DefaultTelemetryConnector()
+		{
+		}
+	}
+}
---
+++ TelemetrySystem/ITelemetryConnector.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Tonino
+ * Date: 11/02/2011
+ * Time: 17:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TDDMicroExercises.TelemetrySystem
+{
+	/// <summary>
+	/// Description of ITelemetryConnector.
+	/// </summary>
+	public interface ITelemetryConnector
+	{
+		
+	}
+}
